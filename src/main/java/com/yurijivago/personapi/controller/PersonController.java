967e0e7d800f2b98cb1f0de0411870eff350fa04
Yuri Jivago <yurijivagoads@gmail.com>
2022-03-04 23:29:23 -0300

    Developed person create operation

--- src/main/java/com/yurijivago/personapi/controller/PersonController.java
+++ src/main/java/com/yurijivago/personapi/controller/PersonController.java
@@ -1,15 +1,26 @@
 package com.yurijivago.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yurijivago.personapi.dto.MessageResponseDTO;
+import com.yurijivago.personapi.entity.Person;
+import com.yurijivago.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API teste!";
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        return personService.createPerson(person);
     }
 }
---
+++ src/main/java/com/yurijivago/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.yurijivago.personapi.service;
+
+import com.yurijivago.personapi.dto.MessageResponseDTO;
+import com.yurijivago.personapi.entity.Person;
+import com.yurijivago.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
